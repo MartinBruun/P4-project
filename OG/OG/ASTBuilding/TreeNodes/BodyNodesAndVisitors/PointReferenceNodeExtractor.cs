d58cd7be3218fdf4bccc743ab036f9cdb7058079
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-04-09 15:30:26 +0200

    Created Added way to return PointfunctionCallNodes in the PointRefExtractor.

--- OG/OG/ASTBuilding/TreeNodes/BodyNodesAndVisitors/PointReferenceNodeExtractor.cs
+++ OG/OG/ASTBuilding/TreeNodes/BodyNodesAndVisitors/PointReferenceNodeExtractor.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Antlr4.Runtime.Tree;
 using OG.ASTBuilding.Draw;
 using OG.ASTBuilding.Terminals;
@@ -9,12 +10,15 @@ namespace OG.ASTBuilding.Shapes
     public class PointReferenceNodeExtractor : OGBaseVisitor<PointReferenceNode>
     {
         private readonly MathNodeExtractor _mathNodeExtractor = new MathNodeExtractor();
+        private ParameterNodeListBuilder _parameterNodeListBuilder;
         public override PointReferenceNode VisitPointReference(OGParser.PointReferenceContext context)
         {
             
             OGParser.NumberTupleContext tupleContext = context.tuple;
             ITerminalNode startPointRefContext = context.StartPointReference();
             ITerminalNode endPointRefContext = context.StartPointReference();
+            OGParser.FunctionCallContext pointFunctionCallContext = context.funcCall;
+
 
             //First element must be id, second must be endPoint or startPoint
             string[] endPointText = endPointRefContext.GetText().Split(".");
@@ -35,6 +39,12 @@ namespace OG.ASTBuilding.Shapes
                 MathNode lhs = _mathNodeExtractor.ExtractMathNode(tupleContext.lhs);
                 MathNode rhs = _mathNodeExtractor.ExtractMathNode(tupleContext.rhs);
                 return new TuplePointNode(context.GetText(), lhs, rhs);
+            }
+            else if (pointFunctionCallContext != null && !pointFunctionCallContext.IsEmpty)
+            {
+                List<ParameterNode> parameters = _parameterNodeListBuilder.VisitFunctionCall(pointFunctionCallContext);
+                IdNode id = new IdNode(pointFunctionCallContext.id.Text);
+                return new PointFunctionCallNode(context.GetText(), id, parameters);
             }
             else
             {
