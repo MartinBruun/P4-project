7233d93f4a225132433c43cf31dc6abc817f6c57
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-04-15 13:02:21 +0200

    yessir

--- OG/OG/AstVisiting/Visitor interfaces/Visitables/x.cs
+++ OG/OG/AstVisiting/Visitor interfaces/Visitables/x.cs
@@ -5,30 +5,5 @@
         public void Accept(IPropertyAssignmentVisitor visitor);
     }
 
-    /*
-    public class ToStringVisitor : IAstVisitor
-    {
-        public void Visit(AdditionNode n)
-        {
-            Console.WriteLine("This is addition___: " + n.ToString());
-        }
-
-        public void Visit(DivisionNode n)
-        {
-            Console.WriteLine(n.ToString());
-        }
-
-        public void Visit(LessThanComparerNode n)
-        {
-            Console.WriteLine(n.ToString());
-        }
-
-        public void Visit(GreaterThanComparerNode n)
-        {
-            Console.WriteLine(n.ToString());
-        }
-
-      
-    }
-    */
+ 
 }
