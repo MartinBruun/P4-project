012d893ce19c1f60a3270098c6445f06c0653563
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-04-30 12:18:43 +0200

    Testing multiple to-chaining from tuples

--- OG/Tests/CodeGenerationTest.cs
+++ OG/Tests/CodeGenerationTest.cs
@@ -17,6 +17,8 @@ namespace Tests
 {
     public class CodeGenerationTest
     {
+        private static Regex G01Regex { get; } = new Regex(@"^G01 X-?\d*\.{0,1}\d+ Y-?\d*\.{0,1}\d+$");
+
         private OGParser CreateParser(string fileName, string dirName)
         {
             Dictionary<string, string> symbolTable = new Dictionary<string, string>();
@@ -69,14 +71,67 @@ namespace Tests
 
             
             result.RemoveAll(string.IsNullOrWhiteSpace);
-            //Regex matching a G01 command without Z coordinate
-            Regex regex = new Regex(@"^G01 X-?\d*\.{0,1}\d+ Y-?\d*\.{0,1}\d+$");
-            
             result.ForEach(str =>
             {
-                Assert.IsTrue(regex.IsMatch(str));
+                Assert.IsTrue(G01Regex.IsMatch(str));
             });
 
         }
+        
+        [TestCase(-0.000001, -0.000001)]
+        [TestCase(-0.01, -0.01)]
+        [TestCase(-0, -1000)]
+        [TestCase(-1000, 0)]
+        [TestCase(-1000, -1000)]
+        [TestCase(1000, -1000)]
+        [TestCase(0, 0)]
+        [TestCase(0.0, 0.2)]
+        [TestCase(10, 10.10)]
+        [TestCase(1, 1)]
+        [TestCase(99999, 99999)]
+        [TestCase(99999, 99999)]
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(-1, -1)]
+        public void Point_Tuples_Should_Give_Equivalent_XY_Value_Using_To_Chaining(double x1, double y1)
+        {
+            NumberNode x1Val = new NumberNode(x1);
+            NumberNode y1Val = new NumberNode(y1);
+            NumberNode x2Val = new NumberNode(x1 + 5);
+            NumberNode y2Val = new NumberNode(y1 + 5);
+
+
+            List<TuplePointNode> tupleNodes = new List<TuplePointNode>();
+
+            for (int n = 0; n < 20; n++)
+            {
+                tupleNodes.Add(n % 2 == 0
+                    ? new TuplePointNode("", new NumberNode(n + 1), new NumberNode(-n - 1))
+                    : new TuplePointNode("", new NumberNode(n - 1), new NumberNode(-n + 1)));
+            }
+
+            
+            List<PointReferenceNode> toCommands = new List<PointReferenceNode>();
+            toCommands.AddRange(tupleNodes);
+            
+            List<string> result = new List<string>();
+            Assert.DoesNotThrow(() =>
+            {
+                LineEmitterVisitor emitter = new LineEmitterVisitor(null, new List<SemanticError>());
+                LineCommandNode lineCommand = new LineCommandNode(new TuplePointNode("", x1Val, y1Val), toCommands);
+
+                lineCommand.Accept(emitter);
+                result = emitter.Emit().Split('\n').ToList();
+            });
+
+            
+            result.RemoveAll(string.IsNullOrWhiteSpace);
+            result.ForEach(str =>
+            {
+                Assert.IsTrue(G01Regex.IsMatch(str));
+            });
+        }
+        
+      
     }
 }
