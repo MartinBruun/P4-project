4b893263b2946d3662c91c628bca642c600fe356
Jakob Skov Søndergård <saxjax@saxjax.dk>
2021-05-05 10:47:36 +0200

    Typechecker3 (#45)
    * type checking parameters in function call, adding AstNode to symboltable
    
    * pretyprinter test er flyttet til test mappen
    
    * a
    
    * Merge branch 'master' of /Users/saxjax/developer/P4-project with conflicts.
    
    * typechecker leder efter problem med params i function call
    
    * fixed large bug that killed us
    
    * vv
    
    * Nu er symboltable og typechecker klar til at blive brugt og gennemtestet, jeg har rettet i diverse extractors og forsøgt at rette så vores tests går igennem, der er dog ting vi ikke kan i vores program: vi kan ikke kalde void funktioner, vi kan ikke tilgå et punkt eller parametre med dot notation ... og der er nok mere.
    
    Co-authored-by: Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>

--- OG/OG/ASTBuilding/TreeNodes/FunctionNode, FunctionCallNodes and extractors/FunctionCalls/ParameterNodeListBuilder.cs
+++ OG/OG/ASTBuilding/TreeNodes/FunctionNode, FunctionCallNodes and extractors/FunctionCalls/ParameterNodeListBuilder.cs
@@ -18,65 +18,102 @@ namespace OG.ASTBuilding.TreeNodes.FunctionCalls
             OGParser.PassedParamsContext parametersContext = context.@params;
             List<ParameterNode> parameters  = new List<ParameterNode>();
             parameters.AddRange(BuildParameterNodeList(parametersContext));
+            
+            foreach (ParameterNode parameterNode in parameters)
+            {
+                if (parameterNode == null)
+                {
+                    //Console.WriteLine("param is NULL!!!!!!");
+                }
+                else
+                {
+                    //Console.WriteLine("Found type: " + parameterNode.ParamType);
+                }
+            }
+
+            //Console.WriteLine("END OF FOREACH 123\n");
+            
             return parameters;
         }
 
         public List<ParameterNode> BuildParameterNodeList(OGParser.PassedParamsContext context)
-        {
+        {   
+            
             List<ParameterNode> parameters  = new List<ParameterNode>();
-            try
+            if (context.ChildCount > 0)
             {
                 try
                 {
+                    try
+                    {
+                        OGParser.MultiParametersContext multipleParams = (OGParser.MultiParametersContext) context;
+                        List<ParameterNode> x = VisitMultiParameters(multipleParams);
+
+                        parameters.AddRange(x);
+                        return parameters;
+                    }
+                    catch (InvalidCastException)
+                    {
+                    }
+
+
                     OGParser.SingleParameterContext singleParam = (OGParser.SingleParameterContext) context;
-
-
-                    parameters.Add(_paramExtractor.VisitSingleParameter(singleParam));
+                    ParameterNode result = _paramExtractor.VisitSingleParameter(singleParam);
+                    //Console.WriteLine(result.Expression?.Value);
+                    parameters.Add(result);
                     return parameters;
-                }
-                catch (InvalidCastException)
-                { }
 
-                try
-                {
-                    OGParser.MultiParametersContext multipleParams = (OGParser.MultiParametersContext) context;
-                    return VisitMultiParameters(multipleParams);
-                 
                 }
-                catch (InvalidCastException)
-                { }
-
-                OGParser.NoParameterContext noParams = (OGParser.NoParameterContext) context;
-                return parameters;
-            }
-            catch (InvalidCastException)
-            {
-                SemanticErrors.Add(new SemanticError(context.Start.Line,context.Start.Column, 
-                    "failed to typecast PassedParamsContext into OGParser.SingleParameterContext, " +
-                    "OGParser.MultiParametersContext and OGParser.MultiParametersContext")
+                catch (Exception e)
                 {
-                    IsFatal = true
-                });
-                return null;
+
+
+                    SemanticErrors.Add(new SemanticError(context.Start.Line, context.Start.Column,
+                        "failed to typecast PassedParamsContext into OGParser.SingleParameterContext, " +
+                        "OGParser.MultiParametersContext and OGParser.MultiParametersContext")
+                    {
+                        IsFatal = true
+                    });
+                    return null;
+                }
+
+                //Console.WriteLine();
+                foreach (ParameterNode param in parameters)
+                {
+                    //Console.Write("Parameters are: " + param.Expression.Value);
+                }
             }
+
+            return parameters;
             
         }
 
+
+        // public virtual ParameterNode makeParamNode(OGParser.PassedParamsContext context)
+        // {
+        //     if (context.ChildCount == 0){
+        //         return new ParameterNode(); 
+        //     }
+        //
+        // }
+        
+
         public override List<ParameterNode> VisitMultiParameters(OGParser.MultiParametersContext context)
         {
-            List<ParameterNode> parameters  = new List<ParameterNode>();
-
+            List<ParameterNode> parameters = new List<ParameterNode>();
             OGParser.PassedParamContext current = context.firstParameter;
+            OGParser.PassedParamsContext nextParams = context.@params;
+            
+            
             if (current != null && !current.IsEmpty)
             {
-               parameters.Add(_paramExtractor.ExtractParameterNode(current));
-               return parameters;
+                parameters.Add(_paramExtractor.ExtractParameterNode(current));
             }
 
-            if (context.@params != null && !context.@params.IsEmpty)
-            {                
-
-                return BuildParameterNodeList(context.@params);
+            if (context.passedParams() != null )
+            {
+                OGParser.PassedParamsContext x = context.passedParams();
+                parameters.AddRange(BuildParameterNodeList(context.@params));
             }
             
             return parameters;
