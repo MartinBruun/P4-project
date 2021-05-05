64556b843cc757aa08b7b52bcb7893f88d5e2f6a
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-05-05 10:57:21 +0200

    Testing for warning notification. SemanticErrors constructor to quicken code speed

--- OG/Tests/CurveCodeGenerationTest.cs
+++ OG/Tests/CurveCodeGenerationTest.cs
@@ -21,7 +21,7 @@ namespace Tests
         private static Regex G03Regex { get; } = new Regex(@"^G03 X-?\d*\.{0,1}\d+ Y-?\d*\.{0,1}\d+ R-?\d*\.{0,1}\d+$");
         
         
-        [TestCase(45, -0.000001, -0.000001, 999999.9999,9999999.9999)]
+        [TestCase(45, -0.0001, -0.0001, 999999.9999,9999999.9999)]
         [TestCase(-45,-0.01, -0.01, 0.01,0.001)]
         [TestCase(-1,-0, -1000, 0, 0)]
         [TestCase(1, -1000, 0,1,0)]
@@ -46,8 +46,60 @@ namespace Tests
                 Assert.IsTrue(curveEmitter.SemanticErrors.Count == 0);
             });
 
-            string result = curveEmitter.Emit();
-            Assert.IsTrue(ArcRegex.IsMatch(result));
+            List<string> gCodeCommands = curveEmitter.Emit().Split("\n").ToList();
+            foreach (string command in gCodeCommands.Where(String.IsNullOrEmpty))
+            {
+                gCodeCommands.Remove(command);
+            }
+
+            Assert.IsTrue(gCodeCommands.Count == 2);
+            Assert.IsTrue(LineRegex.IsMatch(gCodeCommands.First()));
+            Assert.IsTrue(ArcRegex.IsMatch(gCodeCommands.Last()));
         }
+        
+        
+        [TestCase(0, -0.0001, -0.0001, 999999.9999,9999999.9999)]
+        [TestCase(0, -1000, 0,1,0)]
+        [TestCase(0, -1000, -1000,1,0)]
+        [TestCase(0,1000, -1000,100,100)]
+        [TestCase(-0,0, 0,99,99)]
+        [TestCase(-0,10, 10.10, 5,5)]
+        [TestCase(-0,0.0, 0.2, 999,9999)]
+        public void Curve_With_Angle_Zero_Matches_Line_Regex (double angle, double x1, double y1, double x2, double y2)
+        {
+            NumberNode angleNode = new NumberNode(angle);
+            TuplePointNode fromNode = new TuplePointNode("", new NumberNode(x1), new NumberNode(y2));
+            List<PointReferenceNode> toNode = new List<PointReferenceNode>();
+            toNode.Add(new TuplePointNode("", new NumberNode(x2), new NumberNode(y2)));
+            
+            CurveCommandNode curveNode = new CurveCommandNode(fromNode, toNode, angleNode);
+            CurveEmitterVisitor curveEmitter = new CurveEmitterVisitor(new SymbolTable(), new List<SemanticError>());
+            curveNode.Accept(curveEmitter);
+
+            string resultCommand = curveEmitter.Emit().Remove('\n');
+            Assert.IsFalse(resultCommand.Contains("\n") || resultCommand.Contains('\n'));
+            LineRegex.IsMatch(resultCommand);
+        }
+        
+        [TestCase(0, -0.0001, -0.0001, 999999.9999,9999999.9999)]
+        public void Curve_With_Angle_Zero_Gives_Warning (double angle, double x1, double y1, double x2, double y2)
+        {
+            NumberNode angleNode = new NumberNode(angle);
+            TuplePointNode fromNode = new TuplePointNode("", new NumberNode(x1), new NumberNode(y2));
+            List<PointReferenceNode> toNode = new List<PointReferenceNode>();
+            toNode.Add(new TuplePointNode("", new NumberNode(x2), new NumberNode(y2)));
+
+            
+            CurveCommandNode curveNode = new CurveCommandNode(fromNode, toNode, angleNode);
+            CurveEmitterVisitor curveEmitter = new CurveEmitterVisitor(new SymbolTable(), new List<SemanticError>());
+            curveNode.Accept(curveEmitter);
+
+            Assert.IsTrue(curveEmitter.SemanticErrors.Count == 1);
+            Assert.IsTrue(curveEmitter.SemanticErrors.First().IsFatal == false);
+
+
+        }
+        
+        
     }
 }
